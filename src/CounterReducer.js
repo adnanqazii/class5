706fa8c06486f937cc06f3dcd930e5adf85d4d06
adnanqazii <adnanqazii@users.noreply.github.com>
2020-08-10 20:23:36 +0500

    updated

--- src/CounterReducer.js
+++ src/CounterReducer.js
@@ -3,6 +3,8 @@ const counterReducer=(state,action)=> {
     switch(action) {
         case 'INCREMENT':
             return state+1;
+        default:
+            
     }
 }
 export default counterReducer;
